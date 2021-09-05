1cd758f4c9fb2b0d0376193021809462fa2a8273
fanoush <fanoush@gmail.com>
2021-09-05 15:24:20 +0200

    board file

--- espruino/DFU/ID130Color/ID130.py
+++ espruino/DFU/ID130Color/ID130.py
@@ -17,13 +17,9 @@ import pinutils;
 
 info = {
  'name' : "ID130",
- 'link' :  [ "https://github.com/abhigkar/ID107-HR-Plus-Reverse-Engineering" ],
+ 'link' :  [ "https://fccid.io/2AHFTID130PLUSCOLOR" ],
  'espruino_page_link' : 'MDBT42Q',
-  # This is the PCA10036
  'default_console' : "EV_BLUETOOTH",
-# 'default_console_tx' : "D6",
- #'default_console_rx' : "D8",
- 'default_console_baudrate' : "9600",
  'variables' : 2250, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
  'bootloader' : 1, 
  'binary_name' : 'espruino_%v_ID130.hex',
@@ -37,15 +33,6 @@ info = {
      #'DEFINES+=-DCONFIG_GPIO_AS_PINRESET', # Allow the reset pin to work
      'DEFINES+=-DBLUETOOTH_NAME_PREFIX=\'"ID130"\'',
      'DEFINES+=-DUSE_FONT_6X8 -DBLE_HIDS_ENABLED=1',
-#     'JSMODULESOURCES+=libs/js/Font4x4.min.js',
-#     'JSMODULESOURCES+=libs/js/Font4x8Numeric.min.js',
-#     'JSMODULESOURCES+=libs/js/Font4x4Numeric.min.js',
-#     'JSMODULESOURCES+=libs/js/Font6x8.min.js',
-#     'JSMODULESOURCES+=libs/js/Font6x12.min.js',
-#     'JSMODULESOURCES+=libs/js/Font8x12.min.js',
-#     'JSMODULESOURCES+=libs/js/Font8x16.min.js',
-#     'JSMODULESOURCES+=libs/js/FontDennis8.min.js',
-#     'INCLUDE += -I$(ROOT)/libs/id107hp',
      'WRAPPERSOURCES += boards/jswrap_ID130.c',
      'DFU_SETTINGS=--sd-req 129,136 --dev-type 616 --dev-revision 616', ## TODO change for ID107 HR plus
      'NRF_SDK11=1'
